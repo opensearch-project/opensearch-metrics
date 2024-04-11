90c4ae9e0b9f35c9df3e418ce0702f85c2f48286
Brandon Shien <bshien@amazon.com>
2024-04-11 16:12:04 -0700

    Added tests for Cognito and IAM Roles
    Signed-off-by: Brandon Shien <bshien@amazon.com>

--- infrastructure/test/opensearch-stack.test.ts
+++ infrastructure/test/opensearch-stack.test.ts
@@ -24,4 +24,77 @@ test('OpenSearchDomain Stack Test', () => {
     openSearchDomainStackTemplate.resourceCountIs('AWS::Cognito::UserPool', 1);
     openSearchDomainStackTemplate.resourceCountIs('AWS::Cognito::UserPoolGroup', 1);
     openSearchDomainStackTemplate.resourceCountIs('AWS::IAM::Policy', 4);
+    openSearchDomainStackTemplate.hasResourceProperties('AWS::IAM::Role', {
+        "AssumeRolePolicyDocument": {
+            "Statement": [
+                {
+                    "Action": "sts:AssumeRoleWithWebIdentity",
+                    "Condition": {
+                        "StringEquals": {
+                            "cognito-identity.amazonaws.com:aud": {
+                                "Ref": "IdentityPool"
+                            }
+                        },
+                        "ForAnyValue:StringLike": {
+                            "cognito-identity.amazonaws.com:amr": "authenticated"
+                        }
+                    },
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Federated": "cognito-identity.amazonaws.com"
+                    }
+                }
+            ],
+            "Version": "2012-10-17"
+        }
+    });
+    openSearchDomainStackTemplate.resourceCountIs('AWS::Cognito::UserPoolGroup', 1);
+    openSearchDomainStackTemplate.hasResourceProperties('AWS::Cognito::UserPoolGroup', {
+        "GroupName": "opensearch-admin-group",
+        "RoleArn": {
+            "Fn::GetAtt": [
+                "OpenSearchHealthCognitoCognitoopensearchhealthidentitypoolAdminRole3AC37B11",
+                "Arn"
+            ]
+        },
+        "UserPoolId": {
+            "Ref": "UserPool"
+        }
+    });
+    openSearchDomainStackTemplate.resourceCountIs('AWS::Cognito::IdentityPoolRoleAttachment', 1);
+    openSearchDomainStackTemplate.hasResourceProperties('AWS::Cognito::IdentityPoolRoleAttachment', {
+        "IdentityPoolId": {
+            "Ref": "IdentityPool"
+        },
+        "Roles": {
+            "authenticated": {
+                "Fn::GetAtt": [
+                    "OpenSearchHealthCognitoCognitoopensearchhealthidentitypoolAuthRole50009EF2",
+                    "Arn"
+                ]
+            }
+        }
+    });
+
+    openSearchDomainStackTemplate.resourceCountIs('AWS::OpenSearchService::Domain', 1);
+    openSearchDomainStackTemplate.hasResourceProperties('AWS::OpenSearchService::Domain', {
+        "CognitoOptions": {
+            "Enabled": true,
+            "IdentityPoolId": {
+                "Ref": "IdentityPool"
+            },
+            "RoleArn": {
+                "Fn::GetAtt": [
+                    "OpenSearchHealthCognitoAmazonOpenSearchServiceCognitoAccessA34D822B",
+                    "Arn"
+                ]
+            },
+            "UserPoolId": {
+                "Ref": "UserPool"
+            }
+        }
+    });
+
+
+
 });
