c0dbd623b5ae5dcf8f7930ec81d341b558f672b6
Prudhvi Godithi <pgodithi@amazon.com>
2024-06-06 10:00:11 -0700

    Update the release inputs with release versions (#41)
    Signed-off-by: Prudhvi Godithi <pgodithi@amazon.com>

--- src/main/java/org/opensearchmetrics/metrics/release/ReleaseInputs.java
+++ src/main/java/org/opensearchmetrics/metrics/release/ReleaseInputs.java
@@ -4,9 +4,11 @@ public enum ReleaseInputs {
     VERSION_3_0_0("3.0.0", "open", "main"),
     VERSION_2_12_0("2.12.0", "closed", "2.12"),
     VERSION_2_13_0("2.13.0", "closed", "2.13"),
-
-    VERSION_2_14_0("2.14.0", "open", "2.x"),
-    VERSION_1_3_15("1.3.15", "closed", "1.3");
+    VERSION_2_14_0("2.14.0", "closed", "2.14"),
+    VERSION_2_15_0("2.15.0", "open", "2.x"),
+    VERSION_1_3_15("1.3.15", "closed", "1.3"),
+    VERSION_1_3_16("1.3.16", "closed", "1.3"),
+    VERSION_1_3_17("1.3.17", "open", "1.3");
 
     private final String version;
     private final String state;
