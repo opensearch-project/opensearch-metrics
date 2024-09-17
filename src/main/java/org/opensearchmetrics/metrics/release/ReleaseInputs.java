acf026b6448cc727d871de68a7a5ecbd88969d3b
Brandon Shien <44730413+bshien@users.noreply.github.com>
2024-09-17 16:07:12 -0700

    Changed 2.17 to closed (#74)
    Signed-off-by: Brandon Shien <bshien@amazon.com>

--- src/main/java/org/opensearchmetrics/metrics/release/ReleaseInputs.java
+++ src/main/java/org/opensearchmetrics/metrics/release/ReleaseInputs.java
@@ -7,7 +7,7 @@ public enum ReleaseInputs {
     VERSION_2_14_0("2.14.0", "closed", "2.14", false),
     VERSION_2_15_0("2.15.0", "closed", "2.15", true),
     VERSION_2_16_0("2.16.0", "closed", "2.16", true),
-    VERSION_2_17_0("2.17.0", "open", "2.x", true),
+    VERSION_2_17_0("2.17.0", "closed", "2.17", true),
     VERSION_1_3_15("1.3.15", "closed", "1.3", false),
     VERSION_1_3_16("1.3.16", "closed", "1.3", false),
     VERSION_1_3_17("1.3.17", "closed", "1.3", false),
