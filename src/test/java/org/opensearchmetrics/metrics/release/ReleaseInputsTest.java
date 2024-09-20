4acc58cc97359321cd3f82afa20d7d8542f9c1ba
Prudhvi Godithi <pgodithi@amazon.com>
2024-09-20 12:46:01 -0700

    Onboard 2.18.0 release
    Signed-off-by: Prudhvi Godithi <pgodithi@amazon.com>

--- src/test/java/org/opensearchmetrics/metrics/release/ReleaseInputsTest.java
+++ src/test/java/org/opensearchmetrics/metrics/release/ReleaseInputsTest.java
@@ -16,6 +16,7 @@ public class ReleaseInputsTest {
         assertEquals("2.15.0", ReleaseInputs.VERSION_2_15_0.getVersion());
         assertEquals("2.16.0", ReleaseInputs.VERSION_2_16_0.getVersion());
         assertEquals("2.17.0", ReleaseInputs.VERSION_2_17_0.getVersion());
+        assertEquals("2.18.0", ReleaseInputs.VERSION_2_18_0.getVersion());
         assertEquals("1.3.15", ReleaseInputs.VERSION_1_3_15.getVersion());
         assertEquals("1.3.16", ReleaseInputs.VERSION_1_3_16.getVersion());
         assertEquals("1.3.17", ReleaseInputs.VERSION_1_3_17.getVersion());
@@ -32,6 +33,7 @@ public class ReleaseInputsTest {
         assertEquals("closed", ReleaseInputs.VERSION_2_15_0.getState());
         assertEquals("closed", ReleaseInputs.VERSION_2_16_0.getState());
         assertEquals("closed", ReleaseInputs.VERSION_2_17_0.getState());
+        assertEquals("open", ReleaseInputs.VERSION_2_18_0.getState());
         assertEquals("closed", ReleaseInputs.VERSION_1_3_15.getState());
         assertEquals("closed", ReleaseInputs.VERSION_1_3_16.getState());
         assertEquals("closed", ReleaseInputs.VERSION_1_3_17.getState());
@@ -48,6 +50,7 @@ public class ReleaseInputsTest {
         assertEquals("2.15", ReleaseInputs.VERSION_2_15_0.getBranch());
         assertEquals("2.16", ReleaseInputs.VERSION_2_16_0.getBranch());
         assertEquals("2.17", ReleaseInputs.VERSION_2_17_0.getBranch());
+        assertEquals("2.x", ReleaseInputs.VERSION_2_18_0.getBranch());
         assertEquals("1.3", ReleaseInputs.VERSION_1_3_15.getBranch());
         assertEquals("1.3", ReleaseInputs.VERSION_1_3_16.getBranch());
         assertEquals("1.3", ReleaseInputs.VERSION_1_3_17.getBranch());
@@ -64,6 +67,7 @@ public class ReleaseInputsTest {
         assertEquals(true, ReleaseInputs.VERSION_2_15_0.getTrack());
         assertEquals(true, ReleaseInputs.VERSION_2_16_0.getTrack());
         assertEquals(true, ReleaseInputs.VERSION_2_17_0.getTrack());
+        assertEquals(true, ReleaseInputs.VERSION_2_18_0.getTrack());
         assertEquals(false, ReleaseInputs.VERSION_1_3_15.getTrack());
         assertEquals(false, ReleaseInputs.VERSION_1_3_16.getTrack());
         assertEquals(false, ReleaseInputs.VERSION_1_3_17.getTrack());
@@ -74,7 +78,7 @@ public class ReleaseInputsTest {
     @Test
     public void testGetAllReleaseInputs() {
         ReleaseInputs[] releaseInputs = ReleaseInputs.getAllReleaseInputs();
-        assertEquals(12, releaseInputs.length);
+        assertEquals(13, releaseInputs.length);
         assertEquals(ReleaseInputs.VERSION_3_0_0, releaseInputs[0]);
         assertEquals(ReleaseInputs.VERSION_2_12_0, releaseInputs[1]);
         assertEquals(ReleaseInputs.VERSION_2_13_0, releaseInputs[2]);
@@ -82,11 +86,12 @@ public class ReleaseInputsTest {
         assertEquals(ReleaseInputs.VERSION_2_15_0, releaseInputs[4]);
         assertEquals(ReleaseInputs.VERSION_2_16_0, releaseInputs[5]);
         assertEquals(ReleaseInputs.VERSION_2_17_0, releaseInputs[6]);
-        assertEquals(ReleaseInputs.VERSION_1_3_15, releaseInputs[7]);
-        assertEquals(ReleaseInputs.VERSION_1_3_16, releaseInputs[8]);
-        assertEquals(ReleaseInputs.VERSION_1_3_17, releaseInputs[9]);
-        assertEquals(ReleaseInputs.VERSION_1_3_18, releaseInputs[10]);
-        assertEquals(ReleaseInputs.VERSION_1_3_19, releaseInputs[11]);
+        assertEquals(ReleaseInputs.VERSION_2_18_0, releaseInputs[7]);
+        assertEquals(ReleaseInputs.VERSION_1_3_15, releaseInputs[8]);
+        assertEquals(ReleaseInputs.VERSION_1_3_16, releaseInputs[9]);
+        assertEquals(ReleaseInputs.VERSION_1_3_17, releaseInputs[10]);
+        assertEquals(ReleaseInputs.VERSION_1_3_18, releaseInputs[11]);
+        assertEquals(ReleaseInputs.VERSION_1_3_19, releaseInputs[12]);
     }
 
 }
