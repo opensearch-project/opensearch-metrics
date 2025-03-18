ebc6c0c9d9f935b428c35f5f83915dc8c53e1eac
Sayali Gaikawad <gaiksaya@amazon.com>
2025-03-18 14:16:30 -0700

    Add test and fixes
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- src/test/java/org/opensearchmetrics/metrics/MetricsCalculationTest.java
+++ src/test/java/org/opensearchmetrics/metrics/MetricsCalculationTest.java
@@ -152,7 +152,7 @@ public class MetricsCalculationTest {
         when(releaseMetrics.getReleaseLabelPulls(ReleaseInputs.VERSION_2_13_0.getVersion(), "repo1", "open")).thenReturn(3L);
         when(releaseMetrics.getReleaseLabelPulls(ReleaseInputs.VERSION_2_13_0.getVersion(), "repo1", "closed")).thenReturn(8L);
         when(releaseMetrics.getReleaseVersionIncrement(ReleaseInputs.VERSION_2_13_0.getVersion(), "repo1", "main")).thenReturn(true);
-        when(releaseMetrics.getReleaseNotes(ReleaseInputs.VERSION_2_13_0.getVersion(), "repo1", "main")).thenReturn(true);
+        when(releaseMetrics.getReleaseNotes(ReleaseInputs.VERSION_2_13_0.getFullVersion(), "repo1", "main")).thenReturn(true);
         when(releaseMetrics.getReleaseBranch(ReleaseInputs.VERSION_2_13_0.getVersion(), "repo1")).thenReturn(true);
         when(releaseMetrics.getReleaseOwners(ReleaseInputs.VERSION_2_13_0.getVersion(), "repo1")).thenReturn(new String[]{"owner1", "owner2"});
         when(releaseMetrics.getReleaseIssue(ReleaseInputs.VERSION_2_13_0.getVersion(), "repo1")).thenReturn("release-123");
